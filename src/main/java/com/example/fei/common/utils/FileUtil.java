dca36365d8c154c0833e169ae44ede09198b2648
zhaodafei111 <1097625354@qq.com>
2022-05-01 18:10:38 +0800

    no message

--- src/main/java/com/example/fei/common/utils/FileUtil.java
+++ src/main/java/com/example/fei/common/utils/FileUtil.java
@@ -1,12 +1,21 @@
 package com.example.fei.common.utils;
 
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 
 //文件上传工具类
 public class FileUtil {
-    public static void uploadFile(byte[] file,String filePath,String fileName) throws IOException {
+
+    // 文件上传2
+    /*public static void uploadFile2(byte[] file,String filePath,String fileName) throws IOException {
         File targetFile = new File(filePath);
         if (!targetFile.exists()) { // 创建目录
             targetFile.mkdirs();
@@ -16,9 +25,35 @@ public class FileUtil {
         out.write(file);
         out.flush();
         out.close();
-    }
+    }*/
 
-    public static void uploadFile2(InputStream fileStream, String filePath, String fileName) throws IOException {
+    // 采用MultipartFile的transfer() 实现文件上传3
+    /*public static void uploadFile3(MultipartFile uploadFile, String filePath, String fileName) throws IOException {
+        File targetFile = new File(filePath + fileName);
+        uploadFile.transferTo(targetFile);
+    }*/
+
+    // 二进制流文件下载2
+    /*public static ResponseEntity<InputStreamResource> downloadFile2(String fullFilePath) throws IOException {
+        // String fullFilePath  = "E:\\self_web\\git_dev\\vue\\zFei_springBoot\\target\\02.jpg";
+        FileSystemResource file = new FileSystemResource(fullFilePath);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Content-Disposition", String.format("attachment;fileName=%s", file.getFilename()));
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        headers.add("web-file-type", "image/jpeg"); // 自定义文件类型
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentLength(file.contentLength())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(new InputStreamResource(file.getInputStream()));
+    }*/
+
+
+    public static void uploadFile(InputStream fileStream, String filePath, String fileName) throws IOException {
         File targetFile = new File(filePath);
         if (!targetFile.exists()) { // 创建目录
             targetFile.mkdirs();
@@ -35,11 +70,47 @@ public class FileUtil {
         fileStream.close();
     }
 
-    // 采用MultipartFile的transfer() 实现上传
-    public static void uploadFile3(MultipartFile uploadFile, String filePath, String fileName) throws IOException {
-        File targetFile = new File(filePath + fileName);
-        uploadFile.transferTo(targetFile);
+    public static void downloadFile(HttpServletResponse response,String filePath,String fileName) {
+        // String filePath = "E:\\self_web\\git_dev\\vue\\zFei_springBoot\\target\\";
+        // String fileName = "001.png";
+        // String fileName = "02.jpg";
+        response.setContentType("application/octet-stream");
+        response.setHeader("content-type", "application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名  URLEncoder.encode(fileName, "utf-8")
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("web-file-type", "image/jpeg"); // 自定义文件类型
+
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        try {
+            File file = new File(filePath, fileName);
+            byte[] buffer = new byte[1024];
+            fis = new FileInputStream(file);
+            bis = new BufferedInputStream(fis);
+            ServletOutputStream os = response.getOutputStream();
+            int i = bis.read(buffer);
+            while (i != -1) {
+                os.write(buffer, 0, i);
+                i = bis.read(buffer);
+            }
+            System.out.println("success");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (bis != null) {
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
-
-
 }
--- src/main/java/com/example/fei/controller/FileController.java
+++ src/main/java/com/example/fei/controller/FileController.java
@@ -1,46 +1,44 @@
 package com.example.fei.controller;
 
 import com.example.fei.common.utils.FileUtil;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.HashMap;
 
 
 @RestController
 @RequestMapping("/file")
 public class FileController {
 
-    // 上传文件
-    @PostMapping("upload")
-    public void upload(@RequestParam("file") MultipartFile file) {
+    // 上传文件2
+    /*@PostMapping("upload2")
+    public HashMap<String, String> upload2(@RequestParam("file") MultipartFile file) {
 
         // String contentType = file.getContentType(); // 获取文件类型
         String fileName = file.getOriginalFilename(); // 图片名字
         String filePath  = "E:\\self_web\\git_dev\\vue\\zFei_springBoot\\target\\";
 
         try {
-            FileUtil.uploadFile(file.getBytes(), filePath, fileName);
-        } catch (Exception e) {
-            // TODO: 处理异常信息
-        }
-    }
-    // 上传文件2
-    @PostMapping("upload2")
-    public void upload2(@RequestParam("file") MultipartFile uploadFile) {
-
-        String fileName = uploadFile.getOriginalFilename();
-        String filePath  = "E:\\self_web\\git_dev\\vue\\zFei_springBoot\\target\\";
-
-        try {
-            FileUtil.uploadFile2(uploadFile.getInputStream(), filePath, fileName);
+            FileUtil.uploadFile2(file.getBytes(), filePath, fileName);
         } catch (Exception e) {
             // TODO: 处理异常信息
         }
 
-    }
+        return new HashMap<String, String>() {{
+            put("code", "200");
+            put("msg", "上传成功");
+        }};
+    }*/
 
     // 上传文件3
-    @PostMapping("upload3")
-    public void upload3(@RequestParam("file") MultipartFile uploadFile) {
+    /*@PostMapping("upload3")
+    public  HashMap<String, String> upload3(@RequestParam("file") MultipartFile uploadFile) {
 
         String fileName = uploadFile.getOriginalFilename();
         String filePath  = "E:\\self_web\\git_dev\\vue\\zFei_springBoot\\target\\";
@@ -51,10 +49,49 @@ public class FileController {
             // TODO: 处理异常信息
         }
 
+        return new HashMap<String, String>() {{
+            put("code", "200");
+            put("msg", "上传成功");
+        }};
+    }*/
+
+
+    // 下载文件2
+    /*@PostMapping("download2")
+    public ResponseEntity<InputStreamResource> download2() throws IOException {
+        String fullFilePath  = "E:\\self_web\\git_dev\\vue\\zFei_springBoot\\target\\02.jpg";
+        return  FileUtil.downloadFile2(fullFilePath);
+    }*/
+
+
+    // 上传文件
+    @PostMapping("upload")
+    public HashMap<String, String> upload(@RequestParam("file") MultipartFile uploadFile) {
+
+        String fileName = uploadFile.getOriginalFilename();
+        String filePath  = "E:\\self_web\\git_dev\\vue\\zFei_springBoot\\target\\";
+
+        try {
+            FileUtil.uploadFile(uploadFile.getInputStream(), filePath, fileName);
+        } catch (Exception e) {
+            // TODO: 处理异常信息
+        }
+
+        return new HashMap<String, String>() {{
+            put("code", "200");
+            put("msg", "上传成功");
+        }};
     }
 
-    // 下载文件
-    @GetMapping("download")
-    public void download() {
+    // 文件下载
+    @PostMapping("download")
+    public String download2(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String filePath = "E:\\self_web\\git_dev\\vue\\zFei_springBoot\\target\\";
+        // // String fileName = "001.png";
+        String fileName = "02.jpg";
+
+        FileUtil.downloadFile(response,filePath,fileName);
+        return null;
     }
+
 }
