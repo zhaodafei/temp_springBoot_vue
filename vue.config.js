28b3c71d736c385f2683aa6a48cc96c04c8725cc
zhaodafei111 <1097625354@qq.com>
2023-03-03 17:15:22 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   lintOnSave: false,
   publicPath: process.env.NODE_ENV === "production" ? "./." : "/",
+  outputDir: process.env.NODE_ENV === "production" ? "dist" : "dist-dev",
   devServer: {
     host: '0.0.0.0',
     port: 8090,
