8e83e2ce438ef779ff79c7e39f2c5204019a4de3
zhaodafei111 <1097625354@qq.com>
2023-06-13 09:38:21 +0800

    init

--- vite/plugins/html.js
+++ vite/plugins/html.js
@@ -2,12 +2,12 @@
 import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default function createHtml(env) {
-  const { VITE_APP_TITLE } = env
+  const { VITE_APP_TITLE,VITE_APP_ENV } = env
   return createHtmlPlugin({
     inject: {
       data: {
         //将环境变量 VITE_APP_TITLE 赋值给 title 方便 html页面使用 title 获取系统标题
-        title: VITE_APP_TITLE
+        title: VITE_APP_ENV === 'development' ? ('pre' + VITE_APP_TITLE) : VITE_APP_TITLE
       }
     }
   })
