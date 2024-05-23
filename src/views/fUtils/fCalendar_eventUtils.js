acc380ffe94d1c2450fbcc456a1bcfe89de4646c
zhaodafei111 <1097625354@qq.com>
2024-05-23 20:15:33 +0800

    1

--- src/views/fUtils/fCalendar_eventUtils.js
+++ src/views/fUtils/fCalendar_eventUtils.js
@@ -1,20 +1,13 @@
 // PHP后端接口返回的数据格式
 export const API_phpData = [
   {
-    "id": 4,
-    "classroomName": "化学",
-    "startDate": "2024-06-08 08:00:00",
-    "endDate": "2024-06-09 16:23:35",
-    "status": "a03",
-    "location": "北楼203"
-  },
-  {
-    "id": 3,
-    "classroomName": "英语",
-    "startDate": "2024-05-08 08:00:00",
-    "endDate": "2024-05-09 16:23:35",
-    "status": "a03",
-    "location": "北楼203"
+    "id": 1,
+    "classroomName": "数学教室",
+    "startDate": "2024-05-01 16:22:08",
+    "endDate": "2024-05-03 16:22:15",
+    "status": "a01",
+    "location": "主楼M301",
+    "isDel": "0"
   },
   {
     "id": 2,
@@ -22,14 +15,52 @@ export const API_phpData = [
     "startDate": "2024-05-04 16:22:30",
     "endDate": "2024-05-06 16:22:43",
     "status": "a02",
-    "location": "主楼M101"
+    "location": "主楼M101",
+    "isDel": "0"
   },
   {
-    "id": 1,
-    "classroomName": "数学教室",
-    "startDate": "2024-05-01 16:22:08",
-    "endDate": "2024-05-03 16:22:15",
-    "status": "a01",
-    "location": "主楼M301"
+    "id": 3,
+    "classroomName": "英语",
+    "startDate": "2024-05-08 08:00:00",
+    "endDate": "2024-05-09 16:23:35",
+    "status": "a03",
+    "location": "北楼203",
+    "isDel": "0"
+  },
+  {
+    "id": 4,
+    "classroomName": "化学6月",
+    "startDate": "2024-06-08 08:00:00",
+    "endDate": "2024-06-09 16:23:35",
+    "status": "a03",
+    "location": "北楼203",
+    "isDel": "0"
+  },
+  {
+    "id": 5,
+    "classroomName": "物理6月",
+    "startDate": "2024-06-11 08:00:00",
+    "endDate": "2024-06-13 16:23:35",
+    "status": "a03",
+    "location": "北楼203",
+    "isDel": "0"
+  },
+  {
+    "id": 6,
+    "classroomName": "WEB_4月",
+    "startDate": "2024-04-11 08:00:00",
+    "endDate": "2024-04-13 16:23:35",
+    "status": "a03",
+    "location": "西楼W403",
+    "isDel": "0"
+  },
+  {
+    "id": 7,
+    "classroomName": "JAVA_4月",
+    "startDate": "2024-04-11 08:00:00",
+    "endDate": "2024-04-13 16:23:35",
+    "status": "a03",
+    "location": "西楼W403",
+    "isDel": "0"
   }
 ]
